ce26547e31f2407e45e2171d6f079886dbe27399
Vancells dit Bancells <victorbancells@gmail.com>
2019-09-11 14:38:47 +0200

    start

---
+++ Assets/Scripts/LettersButtonsComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LettersButtonsComponent : MonoBehaviour
+{
+    private Button currentBtn;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        currentBtn = GetComponent<Button>();
+    }
+
+    private void Start()
+    {
+        currentBtn.GetComponentInChildren<Text>().text = currentBtn.name;
+    }
+
+    public void CheckLetter()
+    {
+        
+    }
+}
